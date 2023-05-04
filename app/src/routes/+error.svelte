6e07b3e56d1a65e0b1b38799f1355e50a4b00d56
tsuyuri104 <tsuyuri104@gmail.com>
2023-05-04 20:58:15 +0900

    오류화면을 작성했다

---
+++ app/src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { t } from '$lib/translations/translations';
+</script>
+
+<div>
+	<h1>{$page.status}</h1>
+	<p>{$t(`common.error.${$page.status}`)}</p>
+</div>
+
+<style lang="scss">
+	h1 {
+		font-size: 2.5em;
+	}
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
