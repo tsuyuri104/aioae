cbb6d71f54d0ea9e267a7056fec796249b7ec714
tsuyuri104 <tsuyuri104@gmail.com>
2023-05-04 20:14:12 +0900

    언어 설정 쓰는 자리를 바꿨다

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -1,2 +1,17 @@
+//REF: https://github.com/sveltekit-i18n/lib
+import { loadTranslations, locale } from '$lib/translations/translations';
+
 export const prerender = true;
 export const trailingSlash = 'always';
+
+export const load = async ({ url }) => {
+	const { pathname } = url;
+
+	const defaultLocale = 'ko'; // get from cookie, user session, ...
+
+	const initLocale = locale.get() || defaultLocale; // set default if no locale already set
+
+	await loadTranslations(initLocale, pathname); // keep this just before the `return`
+
+	return {};
+};
