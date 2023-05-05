67b7ec1d9cb2c6999f4c74d0b7cbf34a82fd4890
tsuyuri104 <tsuyuri104@gmail.com>
2023-05-05 14:33:12 +0900

    프사와 이름을 표시하는 컴포넌트를 작성했다

---
+++ app/src/components/ProfileMini.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { t } from '$lib/translations/translations';
+</script>
+
+<div class="wrapper">
+	<img src="/images/profile.jpg" alt="profile" class="photo" />
+	<span>{$t('common.nickName')}</span>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		row-gap: 1em;
+	}
+	.photo {
+		width: 150px;
+		border-radius: 25%;
+	}
+</style>
