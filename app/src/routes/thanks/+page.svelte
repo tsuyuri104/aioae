4a03ab9c5138f71299e1931c848e6e2113f4120b
tsuyuri104 <tsuyuri104@gmail.com>
2023-05-13 21:39:10 +0900

    Thanks페이지를 작성했다

---
+++ app/src/routes/thanks/+page.svelte
@@ -0,0 +1,21 @@
+<section>
+	<h1 class="h1">Thanks</h1>
+	<ul>
+		<li class="item">
+			<a href="https://getcssscan.com/css-box-shadow-examples">Beautiful CSS box-shadow examples</a>
+		</li>
+		<li class="item">
+			<a href="https://akveo.github.io/eva-icons/#/">Eva icons</a>
+		</li>
+	</ul>
+</section>
+
+<style lang="scss">
+	.h1 {
+		font-size: 1.6rem;
+	}
+	.item {
+		margin: 0.5rem;
+		padding: 0.5rem;
+	}
+</style>
