719f3e4afc5b8a1b852ecfdabcd80b682876ea9d
tsuyuri104 <tsuyuri104@gmail.com>
2023-05-13 19:50:08 +0900

    푸터를 자성했다

---
+++ app/src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<footer>
+	<p>by Tsuyuri</p>
+</footer>
+
+<style lang="scss">
+	@use '../style/mixins';
+	footer {
+		min-height: 2em;
+		padding: 1.5rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		width: 100vw;
+	}
+</style>
