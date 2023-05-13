7cafda4f5c178d1c8b62afb212d3f0cb1e98ed68
tsuyuri104 <tsuyuri104@gmail.com>
2023-05-13 13:39:55 +0900

    링크 구성을 수정했다

--- app/src/cmstypes/links.ts
+++ app/src/cmstypes/links.ts
@@ -1,10 +1,40 @@
-import type { Common } from './common';
+import {
+	CameraOutlineIcon,
+	EditOutlineIcon,
+	GithubOutlineIcon,
+	HeartOutlineIcon,
+	ShoppingCartOutlineIcon,
+	TwitterOutlineIcon,
+	VideoOutlineIcon
+} from '@robakk08/svelte-evaicons';
+import type { Common, Lang } from './common';
 
 export type Links = Common & {
 	url: string;
-	titleKo: string;
-	titleJa: string;
-	descriptionKo: string;
-	descriptionJa: string;
-	category: string[];
+	title: Lang;
+	description: Lang;
+	category: CategoryTypes;
+	icon: IconTypes;
 };
+
+export type IconTypes =
+	| undefined
+	| 'Twitter'
+	| 'Github'
+	| 'Video'
+	| 'ShoppingCart'
+	| 'Edit'
+	| 'Camera'
+	| 'Heart';
+
+export const IconComponents = new Map<IconTypes, any>([
+	['Twitter', TwitterOutlineIcon],
+	['Github', GithubOutlineIcon],
+	['Video', VideoOutlineIcon],
+	['ShoppingCart', ShoppingCartOutlineIcon],
+	['Edit', EditOutlineIcon],
+	['Camera', CameraOutlineIcon],
+	['Heart', HeartOutlineIcon]
+]);
+
+export type CategoryTypes = undefined | 'SNS' | 'Programming';
