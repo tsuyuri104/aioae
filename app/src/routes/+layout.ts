eb775033c2c9d16270c6c66182194a0fd40db3a0
tsuyuri104 <tsuyuri104@gmail.com>
2023-05-16 22:58:26 +0900

    타입을 썼다

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -1,11 +1,12 @@
 //REF: https://github.com/sveltekit-i18n/lib
 import type { LangType } from '$cmstypes/common';
 import { loadTranslations, locale } from '$lib/translations/translations';
+import type { PageLoad } from './$types';
 
 export const prerender = true;
 export const trailingSlash = 'always';
 
-export const load = async ({ url }) => {
+export const load: PageLoad = async ({ url }) => {
 	const { pathname } = url;
 
 	const defaultLocale: LangType = 'ko'; // get from cookie, user session, ...
