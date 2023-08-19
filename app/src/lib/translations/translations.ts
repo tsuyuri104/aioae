38b10ef350b11260e55240997339b0abb994ecf0
tsuyuri104 <tsuyuri104@gmail.com>
2023-08-19 19:32:52 +0900

    書き方を修正

--- app/src/lib/translations/translations.ts
+++ app/src/lib/translations/translations.ts
@@ -1,19 +1,19 @@
-import i18n from 'sveltekit-i18n';
+import i18n, { type Config } from 'sveltekit-i18n';
 
-/** @type {import('sveltekit-i18n').Config} */
-const config = {
+// /** @type {import('sveltekit-i18n').Config} */
+const config: Config = {
 	loaders: [
 		{
 			locale: 'ko',
 			key: 'common',
-			loader: async () => (await import('./ko/common.json')).default
+			loader: async () => (await import('./ko/common.json')).default,
 		},
 		{
 			locale: 'ja',
 			key: 'common',
-			loader: async () => (await import('./ja/common.json')).default
+			loader: async () => (await import('./ja/common.json')).default,
 		},
-	]
+	],
 };
 
 export const { t, locale, locales, loading, loadTranslations } = new i18n(config);
