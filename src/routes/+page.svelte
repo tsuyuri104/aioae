534bfff596830195471b6922ce402591ef28f7cb
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-10 23:13:52 +0900

    タグコンポーネントを使用するように修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import Tag from '$components/layout/Tag.svelte';
 	import type { LangType } from '$lib/micro-cms/common';
 	import { PROFILE_URL } from '$lib/micro-cms/ogp';
 	import { locale } from '$lib/translations';
-	import { createPageFullTitle } from '$lib/utilities/creater';
 
 	$: localeName = $locale as LangType;
 </script>
 
-<svelte:head>
-	<title>{createPageFullTitle('Top')}</title>
-	<meta property="og:url" content={`${$page.url}`} />
-	<meta property="og:type" content="article" />
-	<meta property="og:title" content={createPageFullTitle('Top')} />
-	<meta property="og:description" content="Tsuyuri's web page" />
-	<meta property="og:site_name" content="by Tsuyuri" />
-	<meta property="og:image" content={`${PROFILE_URL}`} />
-</svelte:head>
+<Tag
+	title="Profile"
+	thambnail={PROFILE_URL}
+/>
