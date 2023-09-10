cdd250c9805938956681e1450d7024124daaab1f
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-10 23:13:14 +0900

    コード整形

--- src/components/layout/Tag.svelte
+++ src/components/layout/Tag.svelte
@@ -8,10 +8,28 @@
 
 <svelte:head>
 	<title>{createPageFullTitle(title)}</title>
-	<meta property="og:url" content={`${$page.url}`} />
-	<meta property="og:type" content="article" />
-	<meta property="og:title" content={createPageFullTitle(title)} />
-	<meta property="og:description" content="Tsuyuri's web page" />
-	<meta property="og:site_name" content="by Tsuyuri" />
-	<meta property="og:image" content={thambnail} />
+	<meta
+		property="og:url"
+		content={`${$page.url}`}
+	/>
+	<meta
+		property="og:type"
+		content="article"
+	/>
+	<meta
+		property="og:title"
+		content={createPageFullTitle(title)}
+	/>
+	<meta
+		property="og:description"
+		content="Tsuyuri's web page"
+	/>
+	<meta
+		property="og:site_name"
+		content="by Tsuyuri"
+	/>
+	<meta
+		property="og:image"
+		content={thambnail}
+	/>
 </svelte:head>
