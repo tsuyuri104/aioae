1d1fb0d7bd1511328bf0643bf199157accc81675
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-11 23:16:00 +0900

    レイアウトを修正

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
 	import Footer from '$components/layout/Footer.svelte';
-	import Header from '$components/layout/Header.svelte';
+	import Nav from '$components/layout/Nav.svelte';
 	import Switcher from '$components/layout/Switcher.svelte';
 	import { firebaseConfig } from '$lib/firebase/config';
 	import { locale } from '$lib/translations';
@@ -19,16 +19,30 @@
 	});
 </script>
 
-<div class="outer">
-	<div class="inner" class:ja={$locale === 'ja'} class:ko={$locale === 'ko'}>
-		<Header />
+<div class="container">
+	<div class="outer">
+		<div
+			class="inner serif"
+			class:ja={$locale === 'ja'}
+			class:ko={$locale === 'ko'}
+		>
+			<main class="main">
+				<slot />
+			</main>
 
-		<main>
-			<slot />
-		</main>
+			<div class="footer">
+				<Footer />
+			</div>
+		</div>
 
-		<Footer />
-		<Switcher />
+		<aside class="side">
+			<div class="nav">
+				<Nav />
+			</div>
+			<div class="switcher">
+				<Switcher />
+			</div>
+		</aside>
 	</div>
 </div>
 
@@ -36,30 +50,78 @@
 	@use 'src/style/fonts';
 	@use 'src/style/colors';
 
+	.container {
+		container-type: inline-size;
+	}
+
 	.outer {
 		background-color: colors.get('beige', 'bg-light');
+		display: flex;
+		flex-direction: column;
 		color: colors.get('beige', 'text');
-		font-size: 16px;
 	}
 
 	.inner {
-		margin-inline: auto;
-		min-height: 100svh;
+		display: flex;
+		flex-direction: column;
+		row-gap: 2em;
 		width: 100svw;
 		max-width: 400px;
+		min-height: 100svh;
 		padding: 1em;
 	}
 
-	@container (400px <= width) {
-		.main {
-			width: 80vw;
+	.side {
+		width: 100%;
+		position: sticky;
+		bottom: 0;
+		left: 0;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.nav {
+		order: 2;
+	}
+
+	.switcher {
+		order: 1;
+		width: fit-content;
+		position: relative;
+		left: 0.5em;
+		bottom: 0.5em;
+	}
+
+	@container (width > 400px) {
+		.outer {
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			column-gap: 1em;
 		}
-	}
+		.inner {
+			order: 2;
+		}
+		.side {
+			display: flex;
+			flex-direction: column;
+			max-width: 150px;
+			position: sticky;
+			order: 1;
+			padding: 1em;
+			row-gap: 1em;
+		}
 
-	}
+		.nav {
+			order: 1;
+		}
 
-	}
-
+		.switcher {
+			order: 2;
+			display: flex;
+			flex-direction: row;
+			justify-content: end;
+			width: 100%;
 		}
 	}
 </style>
