3ce8b2bb1a80c2f7c9e3a2b3d83dc342f4444a36
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-12 22:51:38 +0900

    CommingSoonを作成

---
+++ src/components/layout/CommingSoon.svelte
@@ -0,0 +1,5 @@
+<p class="en sans">Comming Soon...</p>
+
+<style lang="scss">
+	@use 'src/style/fonts';
+</style>
---
+++ src/routes/career/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import CommingSoon from '$components/layout/CommingSoon.svelte';
+</script>
+
+<CommingSoon />
