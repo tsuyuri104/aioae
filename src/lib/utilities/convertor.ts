f5782519713d79bf9e2c390c6d73b0a9d7e15f12
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-16 16:24:25 +0900

    ファイル削除

--- src/lib/utilities/convertor.ts
+++
@@ -1,5 +0,0 @@
-export const toUpperFirst = (value: string): string => {
-	const upper = value.substring(0, 1).toUpperCase();
-	const lower = value.slice(1).toLowerCase();
-	return upper + lower;
-};
