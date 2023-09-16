0d44228e6f990f1b02e711ea3c37de5f5badeaee
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-16 22:42:42 +0900

    リストコンポーネントを作成

---
+++ src/components/common/List.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	export let list: string[];
+</script>
+
+<ul class="ul">
+	{#each list as item (item)}
+		<li class="li">
+			<span class="list-item">{`${item}`}</span>
+		</li>
+	{/each}
+</ul>
+
+<style lang="scss">
+	@use 'src/style/colors';
+
+	.ul {
+		display: flex;
+		flex-direction: column;
+		row-gap: 0.5em;
+	}
+
+	.li {
+		display: list-item;
+		list-style-position: inside;
+		list-style-type: disc;
+		&::marker {
+			color: colors.get('blue', 'text');
+		}
+	}
+
+	.list-item {
+		position: relative;
+		left: -0.5em;
+	}
+</style>
