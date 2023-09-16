531289e0bb4b351eea83c1e5e51a5f9cb52c0c83
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-16 16:04:50 +0900

    テキストリンクコンポーネントを作成

---
+++ src/components/common/TextLink.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let text: string;
+	export let href: string;
+</script>
+
+<a
+	{href}
+	class="link">{text}</a
+>
+
+<style lang="scss">
+	@use 'src/style/fonts';
+	@use 'src/style/colors';
+
+	.link {
+		color: colors.get('blue', 'text');
+		font-weight: 300;
+		padding: 0.5em;
+		border-radius: 0.5em;
+		&:hover {
+			background-color: colors.get('blue', 'hover');
+		}
+	}
+</style>
