7ae95564d7b561714ebc8669db6d9e14a8a13308
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-17 20:21:12 +0900

    ブログ一覧データ取得処理を作成

---
+++ src/routes/api/blog/byOffset/+server.ts
@@ -0,0 +1,15 @@
+import { _getData } from '$api/+server';
+import type { Blog } from '$lib/micro-cms/dao/blog.js';
+import { json } from '@sveltejs/kit';
+
+export async function GET({ url }) {
+	const limit: number = 10;
+	const order: string = '-publishedAt';
+	const offset: number = (Number(url.searchParams.get('page')) ?? 1) * limit;
+
+	// NEXT : タグ検索、カテゴリ検索、ワード検索ができるようにする
+
+	const data = await _getData<Blog>('blog', { limit, order, offset });
+
+	return json(data);
+}
