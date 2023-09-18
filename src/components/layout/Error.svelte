d100213ac5551325d3a7980c6a3c91e2b522e34b
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-18 21:39:18 +0900

    エラーコンポーネントを作成

---
+++ src/components/layout/Error.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { locale, t } from '$lib/translations';
+	import { createPageFullTitle } from '$lib/utilities/creater';
+
+	export let status: number | undefined;
+</script>
+
+<svelte:head>
+	<title>{createPageFullTitle('Error')}</title>
+</svelte:head>
+
+<div
+	class="wrapper serif"
+	class:ja={$locale === 'ja'}
+	class:ko={$locale === 'ko'}
+>
+	<h1 class="h1">{status ?? $page.status}</h1>
+	<p>{$t(`common.error.${status ?? $page.status}`)}</p>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.h1 {
+		font-size: 2.5em;
+	}
+</style>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,29 +1,5 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-	import { locale, t } from '$lib/translations';
-	import { createPageFullTitle } from '$lib/utilities/creater';
+<script>
+	import Error from '$components/layout/Error.svelte';
 </script>
 
-<svelte:head>
-	<title>{createPageFullTitle('Error')}</title>
-</svelte:head>
-
-<div
-	class="wrapper serif"
-	class:ja={$locale === 'ja'}
-	class:ko={$locale === 'ko'}
->
-	<h1 class="h1">{$page.status}</h1>
-	<p>{$t(`common.error.${$page.status}`)}</p>
-</div>
-
-<style lang="scss">
-	.wrapper {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	.h1 {
-		font-size: 2.5em;
-	}
-</style>
+<Error />
