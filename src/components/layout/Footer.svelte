9756d7c0163b2ef55e7903b801e70a7f2635a9c0
tsuyuri104 <tsuyuri104@gmail.com>
2023-09-24 14:44:34 +0900

    フッターを作成

---
+++ src/components/layout/Footer.svelte
@@ -0,0 +1,14 @@
+<footer class="footer en sans">
+	<small>&copy; 2023 Tsuyuri</small>
+</footer>
+
+<style lang="scss">
+	@use 'src/style/colors';
+	@use 'src/style/fonts';
+
+	.footer {
+		background-color: colors.get('beige', 'bg-light');
+		display: flex;
+		justify-content: end;
+	}
+</style>
